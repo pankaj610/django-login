a25d5af8c8a396fae8043f9a47898f15355c2e01
pankaj610 <vermapankaj62@gmail.com>
2019-01-03 22:00:23 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from myapp import views
+
+app_name="myapp"
+urlpatterns = [
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+
+]
